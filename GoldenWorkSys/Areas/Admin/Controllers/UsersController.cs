c8469993f5b8d3651bd4e6b51433d10780ed5848
eihap <eihapinfo@gmail.com>
2023-04-26 14:07:14 +0300

    get data from new user and put it in AspNet user table in database

--- GoldenWorkSys/Areas/Admin/Controllers/UsersController.cs
+++ GoldenWorkSys/Areas/Admin/Controllers/UsersController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using GoldenWorkSys.Models;
+using Microsoft.AspNetCore.Identity;
+using Bl;
+
 namespace GoldenWorkSys.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class UsersController : Controller
     {
+        UserManager<ApplicationUser> _userManager;
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
         public IActionResult Login()
         {
@@ -16,8 +24,34 @@ namespace GoldenWorkSys.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult Register(UsersModel model)
+        public async Task<IActionResult> Register(UsersModel model)
         {
+            if (!ModelState.IsValid) 
+            {
+                return View("Register",model);
+            }
+            ApplicationUser user = new ApplicationUser(){
+                FirstName= model.FirstName,
+                LastName= model.LastName,
+                Email= model.Email,
+                UserName = model.Email 
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            try
+            {
+                if (result.Succeeded)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            catch(Exception ex) 
+            {
+
+            }
             return View(new UsersModel());
         }
     }
